22bedfea1129fcbb773a290091560f4f94e8fa50
Neelam <nsingh@argusoft.in>
2020-03-05 17:26:28 +0530

    Added storage service and also added map view skeleton.

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -3,3 +3,6 @@ export { SettingsService } from './settings.service';
 export { CommonService } from './common.service';
 export { CaseService } from './case.service';
 export { HttpInterceptorService } from './http-interceptor.service';
+export { StorageService } from './storage.service';
+export { DatabaseService } from './database.service';
+export { MapService } from './map.service';
