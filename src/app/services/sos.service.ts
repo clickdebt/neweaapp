74ca9e5481f502868a4faeb72e5ebeb9787c5bc8
Sagar <sjagodra@argusoft.com>
2020-03-30 10:34:31 +0530

    sos changes

--- src/app/services/sos.service.ts
+++ src/app/services/sos.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SosService {
 
-  constructor() { }
+  constructor(
+    public http: HttpClient
+  ) { }
   //TODO
-  sendSOS(storedCaseId, lat, lng): Promise<any> {
-    return null
+  sendSOS(lat, lng) {
+    const apiURL = localStorage.getItem('server_url') + 'b/sms/actions/send_sos_request/' + lat + '/' + lng + '?source=API';
+    console.log(apiURL);
+    return this.http.get(apiURL);
   }
 }
