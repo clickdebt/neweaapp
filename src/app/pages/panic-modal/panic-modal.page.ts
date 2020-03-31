751950ea6477280b7cefe06e4673eb678b673bce
Sagar <sjagodra@argusoft.com>
2020-03-31 17:22:28 +0530

    revert

--- src/app/pages/panic-modal/panic-modal.page.ts
+++ src/app/pages/panic-modal/panic-modal.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { CommonService } from 'src/app/services';
 import { SosService } from 'src/app/services/sos.service';
@@ -10,29 +10,25 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
   styleUrls: ['./panic-modal.page.scss'],
 })
 export class PanicModalPage implements OnInit {
-  progress = 0;
+  progress = 0
   interval;
-  isMuted = false;
-  volumeClass = 'volume-high-sharp';
-  isPause = false;
-  isStart = false;
-  radius = 130;
+  isMuted: boolean = false;
+  volumeClass = "volume-high-sharp"
+  isPause: boolean = false;
+  isStart: boolean = false;
+  radius: number = 130;
+  storedCaseId;
   lat;
-  lng;
-  constructor(
-    private modalCtrl: ModalController,
+  lng
+  constructor(private modalCtrl: ModalController,
     private nativeAudio: NativeAudio,
+    private platform: Platform,
     private commonService: CommonService,
-    private geolocation: Geolocation,
-    private sosService: SosService
-  ) {
-    this.getCurrentLocation();
+    private sosService: SosService,
+    private geolocation: Geolocation) {
+    this.getCurrentLocation()
   }
-
-  ngOnInit() {
-  }
-
-  async ionViewDidEnter() {
+  async ngOnInit() {
     await this.nativeAudio.preloadSimple('score_0', 'assets/audio/10.wav');
     await this.nativeAudio.preloadSimple('score_1', 'assets/audio/9.wav');
     await this.nativeAudio.preloadSimple('score_2', 'assets/audio/8.wav');
@@ -46,91 +42,83 @@ export class PanicModalPage implements OnInit {
     await this.nativeAudio.preloadSimple('score_10', 'assets/audio/0.wav');
     await this.nativeAudio.preloadSimple('siren', 'assets/audio/SOS-alarm.wav');
   }
-
+  ionViewDidEnter() {
+  }
   async dismiss() {
     this.pausePanicCounter();
-    if (this.progress === 10) {
+    if (this.progress == 10)
       await this.nativeAudio.stop('siren');
-    }
     this.modalCtrl.dismiss({
       saved: false
     });
   }
-
   async startPanicCounter() {
     this.isPause = false;
     this.isStart = true;
-    if (!this.isMuted && this.progress == 0) {
+    if (!this.isMuted && this.progress == 0)
       await this.nativeAudio.play('score_0');
-    }
-
     const getProgress = async () => {
       if (this.progress <= 9) {
-        this.progress++;
+        this.progress++
       }
-      if (!this.isMuted && this.progress < 10) {
-        await this.nativeAudio.play('score_' + this.progress);
-      }
+      if (!this.isMuted && this.progress < 10)
+        await this.nativeAudio.play('score_' + this.progress)
       if (this.progress == 10) {
-        this.sendSosRequest();
-        this.isPause = true;
+        this.sendSosRequest()
+        this.isPause = true
         clearInterval(this.interval);
       }
-    };
-
-    this.interval = setInterval(getProgress, 1000);
+    }
+    this.interval = setInterval(getProgress, 1000)
   }
-
   pausePanicCounter() {
-    this.isStart = false;
-    this.isPause = true;
+    this.isStart = false
+    this.isPause = true
     clearInterval(this.interval);
   }
-
   async playSiren() {
-    if (!this.isMuted) {
+    if (!this.isMuted)
       await this.nativeAudio.play('siren');
-    }
   }
-
   async sendSosRequest() {
     await this.playSiren();
-    this.sosService.sendSOS(this.lat, this.lng).subscribe((response: any) => {
-      this.commonService.showToast(response.message);
-    });
+    // this.sosService.sendSOS(this.storedCaseId, this.lat, this.lng).then((responseObj) => {
+    //   this.setSOSTemplate(responseObj);
+    // });
   }
-
+  setSOSTemplate(responseObj) {
+    if (responseObj != undefined) {
+      this.commonService.showToast(responseObj.message);
+    }
+  }
   async changeSoundSetting() {
-    this.isMuted = !this.isMuted;
+    this.isMuted = !this.isMuted
     if (this.isMuted) {
       if (this.progress == 10) {
         await this.nativeAudio.stop('siren');
       }
-      this.volumeClass = 'volume-mute-sharp';
+      this.volumeClass = "volume-mute-sharp";
     } else {
       if (this.progress == 10) {
         await this.playSiren();
       }
-      this.volumeClass = 'volume-high-sharp';
+      this.volumeClass = "volume-high-sharp";
     }
   }
-
   getOverlayStyle() {
-    const isSemi = false;
-    const transform = (isSemi ? '' : 'translateY(-50%) ') + 'translateX(-50%)';
-
+    let isSemi = false;
+    let transform = (isSemi ? '' : 'translateY(-50%) ') + 'translateX(-50%)';
     return {
-      top: isSemi ? 'auto' : '50%',
-      bottom: isSemi ? '5%' : 'auto',
-      color: '#000 !important',
-      left: '50%',
-      transform,
+      'top': isSemi ? 'auto' : '50%',
+      'bottom': isSemi ? '5%' : 'auto',
+      'color': '#000 !important',
+      'left': '50%',
+      'transform': transform,
       '-moz-transform': transform,
       '-webkit-transform': transform,
       'font-size': this.radius / 3.5 + 'px'
     };
   }
-
   async getCurrentLocation() {
     const { coords } = await this.geolocation.getCurrentPosition();
     this.lng = coords.longitude;
