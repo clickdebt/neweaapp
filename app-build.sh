3375963e5eef07287aff08e1465bb9f0f3a96c80
Sagar <sjagodra@argusoft.com>
2020-08-21 18:55:17 +0530

    build

--- app-build.sh
+++ app-build.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
+# this is a script to build and sign the app for play store
+# We are using my-release-key.jks file for self signing the android app
+# enforce.keystore is also a key file but we are not using that as of now
+# we need to change the id and version in confif.xml in the widget tag for new release
+#
 
 time=$(date "+%Y.%m.%d-%H.%M.%S");
 new_name="Enforce-$time.apk";
