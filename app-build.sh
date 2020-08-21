7a510bb01b3d372ebdf2bf73e81faa9550899b18
Sagar <sjagodra@argusoft.com>
2020-08-21 18:55:02 +0530

    build

--- app-build.sh
+++ app-build.sh
@@ -3,10 +3,10 @@
 time=$(date "+%Y.%m.%d-%H.%M.%S");
 new_name="Enforce-$time.apk";
 mv FieldAgent_v3.apk old_builds/$new_name;
-echo "Pass: enforce";
+echo "Pass: lateral";
 . ~/.nvm/nvm.sh;
 nvm use 12;
 ionic cordova build --release android --prod;
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./my-release-key.jks platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name;
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./my-release-key.jks platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk my-alias;
 zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk FieldAgent_v3.apk;
 
