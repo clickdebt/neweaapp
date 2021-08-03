476251216c9de3556763a8050ca5049f519f44ce
prmohanlal <prmohanlal@argusoft.in>
2021-08-03 12:19:35 +0530

    AD-1120 ##comment Updated add fee functionality for manual fee type

--- src/app/pages/add-fee-modal/add-fee-modal.page.ts
+++ src/app/pages/add-fee-modal/add-fee-modal.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CaseActionService } from 'src/app/services/case-action.service';
-import { NavParams, ModalController } from '@ionic/angular';
+import { NavParams, ModalController, AlertController } from '@ionic/angular';
 import { CommonService, DatabaseService, StorageService } from 'src/app/services';
 import * as moment from 'moment';
 
@@ -20,6 +20,7 @@ export class AddFeeModalPage implements OnInit {
     private modalCtrl: ModalController,
     private storageService: StorageService,
     private databaseService: DatabaseService,
+    private alertCtrl: AlertController,
     private commonService: CommonService) {
   }
 
@@ -38,26 +39,47 @@ export class AddFeeModalPage implements OnInit {
     //   this.feeOptions = response.data.data;
     // });
   }
-  onSelectChange(event) {
+  async onSelectChange(event) {
     if (this.selectedFeeOption) {
       var selectedFee = this.feeOptions.filter((item: any) => {
         return item.id == this.selectedFeeOption;
       });
-      const feeData = {
+      var feeData = {
         fee_id: selectedFee[0].id,
         amount: selectedFee[0].amount
       };
-      const api_data = [
-        { name: 'case_id', value: `${this.caseId}` },
-        { name: 'url', value: `b/clickdebt_panel_layout/financial/case_actions_panels/case_action_add_fee/${this.caseId}?source=API`, },
-        { name: 'type', value: `post` },
-        { name: 'data', value: `${encodeURI(JSON.stringify(feeData))}` },
-        { name: 'is_sync', value: 0 },
-        { name: 'created_at', value: `${moment().format('YYYY-MM-DD hh:mm:ss')}` },
-      ]
-      this.caseActionService.saveActionOffline('api_calls', api_data);
-      this.storageService.set('is_case_updated', true);
-      this.dismiss();
+      if(selectedFee[0].feetype == "manual_financial_fee_prototype"){
+        const alert = await this.alertCtrl.create({
+          header: 'Manual Fee Selected',
+          message: 'Please enter the amount as you have selected Manual Fee',
+          inputs: [
+            {
+              name: 'amount',
+              type: 'number',
+            }
+          ],
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              handler: () => {
+                this.dismiss();
+              }
+            },
+            {
+              text: 'Sumbit',
+              handler: (data) => {
+                feeData.amount = data.amount;
+                this.sumbitAction(this.caseId,feeData);
+              }
+            }
+          ]
+        });
+        await alert.present();
+
+      } else {
+        this.sumbitAction(this.caseId,feeData);
+      }
       // this.caseActionService.addFee(feeData, this.caseId).subscribe((response: any) => {
       //   this.getFeeActions();
       //   this.commonService.showToast(response.data.message, 'success');
@@ -66,6 +88,19 @@ export class AddFeeModalPage implements OnInit {
       //   }
       // });
     }
+  }
+  sumbitAction(caseId,feeData){
+    const api_data = [
+      { name: 'case_id', value: `${caseId}` },
+      { name: 'url', value: `b/clickdebt_panel_layout/financial/case_actions_panels/case_action_add_fee/${caseId}?source=API`, },
+      { name: 'type', value: `post` },
+      { name: 'data', value: `${encodeURI(JSON.stringify(feeData))}` },
+      { name: 'is_sync', value: 0 },
+      { name: 'created_at', value: `${moment().format('YYYY-MM-DD hh:mm:ss')}` },
+    ]
+    this.caseActionService.saveActionOffline('api_calls', api_data);
+    this.storageService.set('is_case_updated', true);
+    this.dismiss();
   }
   async getFeeActions() {
     // let query = 'select * from fees where caseid = ?';
